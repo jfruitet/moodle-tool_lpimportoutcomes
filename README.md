bd9379da125097c48870021f7fd91161daffa0d3
jfruitet <jean.fruitet@free.fr>
2016-05-16 23:17:01 +0200

    Commit initial
    Signed-off-by: jfruitet <jean.fruitet@free.fr>

---
+++ README.md
@@ -0,0 +1,29 @@
+Competencies: Outcomes import tool
+==================================
+
+Moodle plugin to import outcomes into a competency framework.
+
+License
+-------
+
+Licensed under the [GNU GPL License](http://www.gnu.org/copyleft/gpl.html).
+
+
+## Français
+
+Cet outil d'administration permet la création de référentiels de compétences à partir
+des Objectifs (outcomes) de Moodle.
+
+### Installation
+
+* Télécharger l'archive ZIP
+* La renommer "lpimportoutcomes"
+* Copier tout le contenu dans le dossier
+
+	./moodle/admin/tool
+
+* Se loger comme "administrateur"
+* Passez par "Administration du site / Notification"
+* Mettre à jour la base de données.
+
+C'est tout !
---
+++ lang/fr/tool_lpimportoutcomes.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package    tool_lpimportoutcomes
+ * @copyright  2016 Frédéric Massart - FMCorz.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['additionalscaleshelp'] = 'Les objectifs sélectionnés partagent plus qu\'un seul barème. Veuillez configurer ci-après chaque barème supplémentaire .';
+$string['continuetoframework'] = 'Poursuivre vers le référentiel';
+$string['createframework'] = 'Créer le référentiel';
+$string['importmore'] = 'Importer plus';
+$string['importoutcomes'] = 'Importer des objectifs';
+$string['invalidoutcomefound'] = 'Objectif invalide trouvé';
+$string['lpimportoutcomes:outcomesimport'] = 'Importet des objectifs';
+$string['nextstep'] = 'Etape suivante ';
+$string['noutcomeschosen'] = '{$a} objectifs (s) sélectionnés';
+$string['pluginname'] = 'Outil d\'importation d\'Objectifs';
+$string['selectoutcomes'] = 'Sélectionnez les objectifs';
+$string['outcomessuccessfullyimported'] = 'Les objectifs ont été importés avec succès.';
